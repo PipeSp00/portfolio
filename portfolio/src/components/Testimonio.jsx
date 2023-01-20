acb0457199957b29fbe1690afb51911470c363a9
Felipe Spena <fspena@TREND-BSAS.COM.AR>
2023-01-20 13:59:03 -0300

    Portfolio

--- portfolio/src/components/Testimonio.jsx
+++ portfolio/src/components/Testimonio.jsx
@@ -1,3 +1,19 @@
 import React from 'react'
 
-function Testimonio () 
+function Testimonio() {
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio' src={"../imagenes/imagen.png"} alt="" />
+      <div className="contenedor-texto-testimonio">
+        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
+        <p className='cargo-0estimonio'>Ingenieria de software en Spotify</p>
+        <p className='texto-testimonio'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, non. Facilis nisi facere consequatur deleniti! Consectetur enim ducimus ad quo quasi doloribus pariatur repellat odit necessitatibus officia, beatae dolor optio.
+        Impedit deleniti nostrum commodi tenetur, natus iste dolor nisi harum soluta aut debitis eligendi, voluptas neque illo expedita! Et repellat cupiditate accusamus officiis distinctio ratione sunt fugit illum officia fuga.
+        A dicta sapiente earum odit voluptas asperiores architecto! Quam ut est, perspiciatis deleniti incidunt ad rerum molestiae nemo blanditiis natus consequuntur a provident laborum vero beatae quo impedit rem recusandae.</p>
+      </div>
+    </div>
+
+  )
+}
+
+export default Testimonio
